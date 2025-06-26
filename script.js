7ea39e7adc8317b989f5a97e3b2d5ba9ebbc2d5e
HenriqueLafa <henriquelafayete39@gmail.com>
2025-06-25 21:23:13 -0300

    padaria

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+window.revelar = ScrollReveal({reset:true});
+
+revelar.reveal(' .efeito-img-topo', { //estou chamando a classe a qual eu quero atribuir o efeito do scroolReveal.
+    duration: 2000,
+    distance:'90px'
+
+
+});
+
+revelar.reveal('.clientes-efeito', {
+duration:2000,
+distance:'120px',
+origin: 'right'  
+
+}) 
+
+revelar.reveal ('.principais-produtos',   {
+duration:2000,
+distance:'120px',
+origin:'left'
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
 //hamburgerButton
 //Aqui adicionamos um evento de escutar o click dou mouse sobre o icone de hamburguer
 hamburgerButton.addEventListener("click", function() {
